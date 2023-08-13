9042d7e5428c187cc64e51778b07ab2b09f5a3c2
arifardia <arifard49@gmail.com>
2023-08-13 16:59:16 +0700

    commit

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Arif Ardiansyah",
 	url: "https://arfh.site/",
 	language: "id",
-	description: "I am writing about my experience.",
+	description: "Personal Site of Arif Ardiansyah",
 	author: {
 		name: "Arif Ardiansyah",
-		email: "youremailaddress@example.com",
+		// email: "youremailaddress@example.com",
 		url: "https://arfh.site/"
 	}
 }
